4afc544dfd72c1ecb0be1cbd63cceec33dc89cad
SiphoChris <siphonkebe@gmail.com>
2024-08-01 12:56:28 +0200

    updates

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-//some comment
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { config } from "dotenv";
+config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+const port = +process.env.PORT || 4000;
+
+app.use(express.static('./static'))
+
+app.get("/", (_req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "./static/index.html"));
+});
+app.get("/about", (_req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "./static/about.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
